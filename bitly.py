1ecba7dd7e7df779890287228d2dd81809211abb
igor@igor-Lenovo-B590 <igor.udovenko2012@meta.ua>
2022-09-26 23:54:21 +0300

    bitly.py  delete class and edit requirements.txt

--- bitly.py
+++ bitly.py
@@ -4,60 +4,59 @@ import requests
 import os
 from dotenv import load_dotenv
 
-load_dotenv()
-
 logging.basicConfig(
     filename="exc_bitly.log", format="[%(asctime)s] [%(levelname)s] => %(message)s]"
 )
 
 
-class BitlyLinks:
-    def __init__(self, token: str) -> None:
-        self.url = input("Введите ссылку  : ")
-        self.token = token
-        self.headers = {"Authorization": self.token}
-
-    def __shorten_link(self) -> str:
-        try:
-            data = {"long_url": self.url}
-            response = requests.post(
-                "https://api-ssl.bitly.com/v4/bitlinks",
-                headers=self.headers,
-                data=json.dumps(data),
-            )
-            response.raise_for_status()
-
-            return f'Битлинк: {response.json()["id"]}'
-
-        except requests.exceptions.RequestException as exc:
-            logging.exception("%s", exc.__doc__)
-            return f"ERROR: {exc.__doc__} > Проверьте введенные данные!"
-
-    def __count_clicks(self) -> str:
-        params = {"units": -1}
-        try:
-            response = requests.get(
-                f"https://api-ssl.bitly.com/v4/bitlinks/{self.url}/clicks/summary",
-                params=params,
-                headers=self.headers,
-            )
-            response.raise_for_status()
-            return f'По вашей ссылке прошли: {response.json()["total_clicks"]}раз(а)'
-
-        except requests.exceptions.RequestException as exc:
-            logging.exception("%s", exc.__doc__)
-            return f"ERROR: {exc.__doc__} > Проверьте введенные данные!"
-
-    def is_bitlink(self) -> str:
-        response = requests.get(
-            f"https://api-ssl.bitly.com/v4/bitlinks/{self.url}", headers=self.headers
+def shorten_link(url: str, token: str) -> str:
+    try:
+        data = {"long_url": url}
+        response = requests.post(
+            "https://api-ssl.bitly.com/v4/bitlinks",
+            headers={"Authorization": token},
+            data=json.dumps(data),
         )
-        if response.ok:
-            return self.__count_clicks()
-        return self.__shorten_link()
+        response.raise_for_status()
+
+        return f'Битлинк: {response.json()["id"]}'
+
+    except requests.exceptions.RequestException as exc:
+        logging.exception("%s", exc.__doc__)
+        return f"ERROR: {exc.__doc__} > Проверьте введенные данные!"
 
 
-token = os.getenv("TOKEN")
+def count_clicks(bitlink: str, token: str) -> str:
+    params = {"units": -1}
+    try:
+        response = requests.get(
+            f"https://api-ssl.bitly.com/v4/bitlinks/{bitlink}/clicks/summary",
+            params=params,
+            headers={"Authorization": token},
+        )
+        response.raise_for_status()
+        return f'По вашей ссылке прошли: {response.json()["total_clicks"]}раз(а)'
+
+    except requests.exceptions.RequestException as exc:
+        logging.exception("%s", exc.__doc__)
+        return f"ERROR: {exc.__doc__} > Проверьте введенные данные!"
+
+
+def is_bitlink(url: str, token: str) -> str:
+    response = requests.get(
+        f"https://api-ssl.bitly.com/v4/bitlinks/{url}", headers={"Authorization": token}
+    )
+    if response.ok:
+        return count_clicks(url, token)
+    return shorten_link(url, token)
+
+
+def main() -> None:
+    token = os.getenv("BITLY_TOKEN")
+    url = input("Введите ссылку  : ")
+    print(is_bitlink(url, token))
+
+
 if __name__ == "__main__":
-    b = BitlyLinks(token)
-    print(b.is_bitlink())
+    load_dotenv()
+    main()
